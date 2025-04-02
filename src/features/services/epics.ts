c96ab123706c8780299032e62730025126307710
nikitaSB01 <boss.shvabauer@bk.ru>
2025-04-02 14:51:38 +0500

    правил epics and main

--- src/features/services/epics.ts
+++ src/features/services/epics.ts
@@ -1,20 +1,26 @@
 import { ofType } from "redux-observable";
 import { catchError, map, mergeMap, of } from "rxjs";
 import { ajax } from "rxjs/ajax";
+import { Observable } from "rxjs";
+import { AnyAction } from "redux";
+
 import {
   fetchServices,
   fetchServicesSuccess,
   fetchServicesFailure,
 } from "./servicesSlice";
+import { Service } from "./types";
 
-const API_URL = "https://httpserver-1-74d4.onrender.com/api/services"; // <-- твой URL
+const API_URL = "https://react-redux-saga-two-bac.onrender.com/api/services";
 
-export const servicesEpic = (action$: any) =>
+export const servicesEpic = (
+  action$: Observable<AnyAction>
+): Observable<AnyAction> =>
   action$.pipe(
     ofType(fetchServices.type),
     mergeMap(() =>
-      ajax.getJSON(API_URL).pipe(
-        map((response) => fetchServicesSuccess(response as any)),
+      ajax.getJSON<Service[]>(API_URL).pipe(
+        map((response) => fetchServicesSuccess(response)),
         catchError((error) =>
           of(fetchServicesFailure(error.message || "Ошибка запроса"))
         )
